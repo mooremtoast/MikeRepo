9b3ec30b0e10652133bcceb61961d23ca299db78
mooremtoast <moorem@toast.co.uk>
2014-08-13 14:31:21 +0100

    frooter/header

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+  <p>Footer</p>
+  <p><time pubdate datetime="2014-08-01"></time></p>
+</footer>
---
+++ header.php
@@ -0,0 +1,5 @@
+<header class="header_bg">
+  <div class="row-fluid padd">
+    <h1>Codeify - Professional Web Development</h1>
+  </div>
+</header>
